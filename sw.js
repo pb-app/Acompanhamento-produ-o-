faebdb6bdfa692cbd8bf3d43ce6acf100a405310
pb-app <uegafs@gmail.com>
2025-11-26 05:03:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'producao-app-v1';
+const CACHE_NAME = 'producao-app-v2';
 
 // Lista de arquivos para salvar no celular (Cache)
 // IMPORTANTE: Os nomes aqui devem ser IDÊNTICOS aos arquivos do GitHub
